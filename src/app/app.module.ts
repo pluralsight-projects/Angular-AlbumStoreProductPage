4ac7c18894c9334dad1d8003aa13d3385773d290
Kelani Tolulope <kelanik8@gmail.com>
2019-10-23 17:56:49 +0100

    app upadtes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,13 @@ import { ProductDescriptionComponent } from './product-description/product-descr
 import { ProductService } from './product.service';
 import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
 import { ProductListComponent } from './product-list/product-list.component';
-
+import {RouterModule, Routes} from '@angular/router';
+// import { Route } from '@angular/compiler/src/core';
+const appRoutes : Routes = [
+  { path: 'products', component: ProductListComponent },
+  { path: 'product/:id', component: ProductPageComponent },
+  { path: '', redirectTo: 'products' }
+]
 @NgModule({
   declarations: [
     AppComponent,
@@ -21,7 +27,8 @@ import { ProductListComponent } from './product-list/product-list.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
