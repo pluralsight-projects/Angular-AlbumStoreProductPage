312a693b75875b15a511c92b280917fc4104dcba
dallintine <dallintine@yahoo.ca>
2019-10-26 16:58:27 +0100

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
@@ -21,3 +22,28 @@ import { ProductService } from './product.service';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+=======
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { ProductPageComponent } from './product-page/product-page.component';
+import { ProductService } from './product.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [ProductService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+>>>>>>> refs/remotes/origin/master
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,12 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
+<<<<<<< HEAD
+import { Album } from './album';
+import { Observable } from './rxjs/observable';
+
+=======
+>>>>>>> refs/remotes/origin/master
 
 @Injectable()
 export class ProductService {
@@ -8,8 +14,14 @@ export class ProductService {
 
   constructor(private _http: Http) { }
 
+<<<<<<< HEAD
+  getAlbum(id: number): Observable<Album> {
+    return this._http.get(this._albumUrl).map((response) =>
+    <Album>response.json());
+=======
   getAlbum(id: number) {
     return this._http.get(this._albumUrl).map((response) =>
     response.json());
+>>>>>>> refs/remotes/origin/master
   }
 }
