815d6d39b5addd389bcaf9807c67399c6a7847be
Busry <minitext@gmail.com>
2019-11-02 20:41:43 +0000

    add interface for data type purpose

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,3 +1,4 @@
+import { Album } from "../album";
 import { Component, OnInit } from "@angular/core";
 import { ProductService } from "../product.service";
 
@@ -7,10 +8,12 @@ import { ProductService } from "../product.service";
   styleUrls: ["./product-description.component.css"]
 })
 export class ProductDescriptionComponent implements OnInit {
-  albumInfo;
+  albumInfo: Album;
   constructor(private _productService: ProductService) {}
 
   ngOnInit() {
-    this._productService.getAlbum(1).subscribe( response => this.albumInfo = response );
+    this._productService
+      .getAlbum(1)
+      .subscribe(response => (this.albumInfo = response));
   }
 }
