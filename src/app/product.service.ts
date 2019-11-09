6c9591d989ffc86e3e1407ec5f51ca41e823650a
livelinksinfoe <livelinksinfographix@gmail.com>
2019-11-09 22:05:48 +0100

    Completed module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -7,15 +7,28 @@ import { Album } from './album';
 
 import { Observable} from 'rxjs/Observable';
 
+import { Product } from './product';
+
+import { AlbumStorePage } from '../../e2e/app.po';
+
 @Injectable()
 export class ProductService {
+  // getProducts() {
+  //   throw new Error("Method not implemented.");
+  // }
 private _albumUrl = '../assets/album.json';
+private _productsUrl = '../assets/products.json'
 
   constructor(private _http: Http) { }
 
   getAlbum(id: number): Observable<Album> {
     return this._http.get(this._albumUrl).map((response) =>
     <Album>response.json());
+
+  }
+  getProducts() {
+    return this._http.get(this._productsUrl).map(response => 
+      <Product[]>response.json());
   }
 }
  
