8809862930ab4b5afa342532a7db8fd5280a3782
Bryson Nyamwange <brysonnyamwange@gmail.com>
2020-09-28 15:42:49 +0300

    Creating a Service to Get Album Data

---
+++ src/app/album.ts
@@ -0,0 +1,5 @@
+export interface Album {
+  id: number;
+  artistName: string;
+  albumName: string;
+}
--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Injectable, OnInit } from '@angular/core';
+import {ProductService} from '../product.service';
+import {album} from '../album';
 @Component({
   selector: 'app-product-description',
   templateUrl: './product-description.component.html',
   styleUrls: ['./product-description.component.css']
 })
-export class ProductDescriptionComponent implements OnInit {
 
-  constructor() { }
+export class ProductDescriptionComponent implements OnInit {
+  albumInfo: album;
+
+  constructor(private _productService) { }
 
   ngOnInit() {
   }
+  this._productService.getAlbum(1).subscribe(response => this.albumInfo = response)
 
 }
