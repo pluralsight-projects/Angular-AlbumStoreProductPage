cfd6599586580c30861a635af7e8cc5e6288e42a
yahayakenny <y.hkehinde@yahoo.com>
2020-09-28 15:29:42 +0100

    Completed module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
+import {Http, Response} from '@angular/http'
 
 @Injectable()
 export class ProductService {
 
-  constructor() { }
+  constructor(private _http:Http) { }
 
 }
