cef16e6f53284089794b73c0dc3c85aeedd7ce9d
kiminzajnr <kiminzaeric@gmail.com>
2020-10-02 17:52:58 +0300

    Using Interfaces to Describe Data

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -13,6 +13,6 @@ export class ProductService {
 
   }
   getAlbum(id: number): Observable<Album>{
-    return this._http.get(this._albumUrl).map((response)=><Album>response.json());
+    return this._http.get(this._albumUrl).map((response)=><Album>response.json())
   }
 }
