21c3761ad6d24cab3949221be981dd10c49ee7bb
kiminzajnr <kiminzaeric@gmail.com>
2020-10-06 16:51:54 +0300

    creating navigation using the angular router

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,5 +1,6 @@
 import 'rxjs/add/operator/map';
 import { Album } from './album';
+import { Product } from './product';
 import { Observable } from 'rxjs/Observable';
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
@@ -8,11 +9,15 @@ import { Http, Response } from '@angular/http';
 
 export class ProductService {
   private _albumUrl="../assets/album.json";
+  private _productsUrl="../assets/products.json";
 
   constructor(private _http: Http) {
 
   }
   getAlbum(id: number): Observable<Album>{
     return this._http.get(this._albumUrl).map((response)=><Album>response.json())
+  }
+  getProducts(): Observable<Product[]>{
+    return this._http.get(this._productsUrl).map((response)=><Product[]>response.json())
   }
 }
