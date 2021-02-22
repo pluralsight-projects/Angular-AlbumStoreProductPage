b8e71f44097655c77da943a217c475b6167c7921
Dmitri Karpov <developer609@gmail.com>
2021-02-21 22:44:10 -0500

    Completed module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,19 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ProductPageComponent } from './product-page/product-page.component';
-import { HttpClient } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
+import { ProductDescriptionComponent } from './product-description/product-description.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductPageComponent
+    ProductPageComponent,
+    ProductDescriptionComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClient
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
